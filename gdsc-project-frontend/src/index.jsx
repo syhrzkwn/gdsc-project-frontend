87a41a1f6b0a4356c416b61bf5f9b663e861629e
Syahir Zakwan <syahirzakwan.dev@gmail.com>
2023-03-28 08:34:32 +0800

    Update css path

--- gdsc-project-frontend/src/index.jsx
+++ gdsc-project-frontend/src/index.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App'
 import Footer from './components/Footer'
-import './assets/css/App.css'
-import './assets/css/index.css'
+import './assets/css/styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
