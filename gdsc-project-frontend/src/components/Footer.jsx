b822965d4d40f42140c63047314071bcd214f8b2
Syahir Zakwan <syahirzakwan.dev@gmail.com>
2023-03-28 08:34:43 +0800

    Update footer detail

--- gdsc-project-frontend/src/components/Footer.jsx
+++ gdsc-project-frontend/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return(
     <div className="footer">
-      <small>&copy; {new Date().getFullYear()} GDSC UiTM. All Rights Reserved.</small>
+      <small>&copy; {new Date().getFullYear()} GDSC UiTM. All Rights Reserved. | Google 2023 Solution Challenge. | <a href="https://github.com/syhrzkwn/gdsc-project-frontend" target="_blank">Source code <i className="bi bi-github"></i></a></small>
     </div>
   )
 }
